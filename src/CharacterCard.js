174f80fbd524406f34570bd66efb4f1b417006de
shuwern <wernzkung@gmail.com>
2018-09-20 10:58:12 +0700

    activationHandler

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -9,6 +9,7 @@ export default class CharacterCard extends Component {
 
     activate = () => {
         if(!this.state.active){
+            this.props.activationHandler(this.props.value)
             this.setState({active: true})
         }
     }

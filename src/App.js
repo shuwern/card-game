9935efdfb4fe160ea61db59242f38ec4eada8522
shuwern <wernzkung@gmail.com>
2018-09-20 10:25:28 +0700

    loop create component from word

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
 
+const word = "Hello"
 class App extends Component {
   render() {
     return (
       <div>
-        <CharacterCard value="h"/>
-        <CharacterCard value="i"/>
+        {
+          Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+        }
       </div>
     );
   }

42cccc45b52ffbc7941b497478a42d686e22a45f
shuwern <wernzkung@gmail.com>
2018-09-22 14:28:29 +0700

    reset when lose

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 export default class CharacterCard extends Component {
     constructor(props){
         super(props)
@@ -11,6 +11,7 @@ export default class CharacterCard extends Component {
         if(prevProps.attempt != this.props.attempt){
             this.setState({active: false})
         }
+        console.log(this.props.attempt)
     }
 
     activate = () => {
@@ -18,11 +19,46 @@ export default class CharacterCard extends Component {
             this.props.activationHandler(this.props.value)
             this.setState({active: true})
         }
+        console.log(this.props.attempt);
     }
 
     render(){
         let className = `card ${this.state.active ? 'activeCard': ''}`
         return (
+            <div className={className} onClick={this.activate}>
+                {this.props.value}
+            </div>
+        )
+    }
+}
+*/
+import React, { Component } from 'react';
+
+export default class CharacterCard extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            active : false
+        }
+    }
+
+    activate = () => {
+        if(!this.state.active){
+            this.props.activationHandler(this.props.value)
+            this.setState({active: true})
+            
+        }
+    }
+
+    componentDidUpdate(prevProps){
+        if(prevProps.attempt !== this.props.attempt){
+          this.setState({active:false})
+        }
+    }
+
+    render(){   
+        let className =`card ${this.state.active ? 'activeCard' : ''}`
+        return(
             <div className={className} onClick={this.activate}>
                 {this.props.value}
             </div>
--- src/WordCard.js
+++ src/WordCard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 import CharacterCard from './CharacterCard';
 import _ from 'lodash';
 
@@ -44,10 +44,60 @@ export default class WordCard extends Component{
                 {
                     Array.from(this.state.chars).map((c, i) => <CharacterCard value={c} key={i} attempt={this.state.attempt} activationHandler={this.activationHandler}/>)
                 }
-                <p class="center">{this.state.completed ? "You Win!" : ""}</p>
-                <p class="center">{this.state.lose ? "You lose" : ""}</p>
+                <p>{this.state.completed ? "You Win!" : ""}</p>
+                <p>{this.state.lose ? "You lose" : ""}</p>
             </div>
         )
 
+    }
+}
+*/
+import CharacterCard from "./CharacterCard";
+import React, { Component } from 'react';
+import './App.css';
+import _ from 'lodash';
+
+const prepareStateFromWord = (given_word) => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return {
+        word,
+        chars,
+        attempt: 1,
+        guess: [],
+        completed: false
+    }
+}
+
+export default class WordCard extends Component {
+    constructor(props){
+        super(props)
+        this.state = prepareStateFromWord(this.props.value)
+    }
+
+    activationHandler = (c) => {
+        let guess = [this.state.guess]+c
+        this.setState({guess})
+        if(guess.length === this.state.chars.length){
+            if(guess === this.state.word){
+                this.setState({guess: [], completed: true})
+            }else{
+                this.setState({guess: [], attempt: this.state.attempt + 1})
+     
+            }
+        }
+    }
+
+    render(){         
+        return(
+            <div className="App">
+                {
+                    Array.from(this.state.chars).map(
+                        (c, i) => <CharacterCard value = {c} key = {i} attempt={this.state.attempt}
+                        activationHandler = {this.activationHandler}/>
+                    )
+                }
+            </div>
+        )
     }
 }

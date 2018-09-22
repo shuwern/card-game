5ae14ea1340a13701dd753528c50fa2406d33966
shuwern <wernzkung@gmail.com>
2018-09-22 23:21:21 +0700

    random word

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "hello"
+let words = ["hello", "world", "digital", "dinosaur", "faith", "original", "water", "drink", "music", "spider"]
+let word = words[Math.floor(Math.random()*words.length)]
 class App extends Component {
   render() {
     return (
